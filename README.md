f547412543b2d0eddb6b787e9b3932be802f33d7
DXC-Security <jphillips66@dxc.com>
2019-06-08 18:12:42 +0000

    the first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# DXC-Git
+A script to easily search for leaked sensitive data belonging to DXC (aswell as HPE and CSC).
+<br>
+<br>This script was written by:<br> https://twitter.com/Daley
+<br>https://github.com/0days
+
+All Credit Goes To Him, in fact, the entire creation of this project is credited to him. By reporting sensitive information found in public repos
+
+## Setup
+```
+pip3 install -r requirements.txt  
+```
+## Usage
+```
+python main.py -h  -- help  
+python main.py -dxc, --dxc  -- scan for dxc assets  
+python main.py -csc, --csc  -- scan for csc assets   
+python main.py -hpe, --hpe  -- scan for hpe assets   
+python main.py -full, --full  -- scan for all assets   
+```
+<hr>
+
+![dxc-git](https://i.imgur.com/KB6nhmP.gif)
---
+++ main.py
@@ -0,0 +1,102 @@
+import sys
+import argparse
+from github import Github
+import time
+
+banner = """
+
+        ██████╗ ██╗  ██╗ ██████╗  ██████╗ ██╗████████╗
+        ██╔══██╗╚██╗██╔╝██╔════╝ ██╔════╝ ██║╚══██╔══╝
+        ██║  ██║ ╚███╔╝ ██║█████╗██║  ███╗██║   ██║
+        ██║  ██║ ██╔██╗ ██║╚════╝██║   ██║██║   ██║
+        ██████╔╝██╔╝ ██╗╚██████╗ ╚██████╔╝██║   ██║
+        ╚═════╝ ╚═╝  ╚═╝ ╚═════╝  ╚═════╝ ╚═╝   ╚═╝
+                                    daleybee@pm.me
+         help: {} -h  
+         
+   Please be aware of GitHub rate limits. Use in intervals.                      
+""".format(sys.argv[0])
+
+print(banner)
+# so users can read warning about rate limits in banner
+
+
+# tools arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("-dxc", "--dxc", help="scan for dxc assets",
+                    action="store_true")
+parser.add_argument("-csc", "--csc", help="scan for csc assets",
+                    action="store_true")
+parser.add_argument("-hpe", "--hpe", help="scan for hpe assets",
+                    action="store_true")
+parser.add_argument("-full", "--full", help="scan for all assets",
+                    action="store_true")
+parser.add_argument("-io", "--io", help="scan dxcdigital.io",
+                     action="store_true")
+args = parser.parse_args()
+
+# to search through all public repos, we must first authenticate to github
+github_key = input("Enter GitHub Token:\n") 
+
+time.sleep(5)
+
+if args.io:
+    print(" Loading endpoints and initializing Git client..")
+    # auth
+    g = Github(github_key)
+    #load endpoints
+    with open('dxc_io_endpoints.config') as f:
+        for line in f:
+            repositories = g.search_code(query=line)
+            for repo in repositories:
+                response = repo.html_url
+                print("\n [!] Potentially sensitive data found: " + response)  
+
+if args.dxc:
+    print(" Loading endpoints and initializing Git client...")
+    # auth
+    g = Github(github_key)
+    # load endpoints
+    with open('dxc_endpoints.config') as f:
+        for line in f:
+            repositories = g.search_code(query=line)
+            for repo in repositories:
+                response = repo.html_url
+                print("\n [!] Potentially sensitive data found: " + response)
+
+if args.csc:
+    print(" Loading endpoints and initializing Git client...")
+    # auth
+    g = Github(github_key)
+    # load endpoints
+    with open('csc_endpoints.config') as f:
+        for line in f:
+            repositories = g.search_code(query=line)
+            for repo in repositories:
+                response = repo.html_url
+                print("\n [!] Potentially sensitive data found: " + response)
+
+if args.hpe:
+    print(" Loading endpoints and initializing Git client...")
+    # auth
+    g = Github(github_key)
+    # load endpoints
+    with open('hpe_endpoints.config') as f:
+        for line in f:
+            repositories = g.search_code(query=line)
+            for repo in repositories:
+                response = repo.html_url
+                print("\n [!] Potentially sensitive data found: " + response)
+
+if args.full:
+    print(" Loading endpoints and initializing Git client...")
+    # auth
+    g = Github(github_key)
+    # load endpoints
+    with open('all_endpoints.config') as f:
+        for line in f:
+            repositories = g.search_code(query=line)
+            for repo in repositories:
+                response = repo.html_url
+                print("\n [!] Potentially sensitive data found: " + response)
+
